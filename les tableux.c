88249150cc1e96be6b0e01aeb086ca9b00a70d53
Achchaimae khalaf <92895971+Achchaimae@users.noreply.github.com>
2022-09-12 17:20:02 +0100

    Update les tableux.c

--- les tableux.c
+++ les tableux.c
@@ -1,29 +1,63 @@
 #include <stdio.h>
 int main()
 {
-    int nbr, i, j;
+    int nbr, i, j,max,position,min,k,tmp;
   int tab1[100], tab2[100];
 
-    printf("Entrez le nombre d'ÈlÈments dans le tableau: ");
+    printf("Entrez le nombre d'√©l√©ments dans le tableau: ");
     scanf("%d", &nbr);
 
-    printf("Entrez les ÈlÈments du tableau: ");
+    printf("Entrez les √©l√©ments du tableau: ");
     for (i = 0; i < nbr ; i++){
             printf("table[%d]:",i+1);
     scanf("%d", &tab1[i]);
 }
-    //Copier les ÈlÈments dans le tableau tab2 ‡ partir de la fin du tableau tab1
+    //Copier les √©l√©ments dans le tableau tab2 √† partir de la fin du tableau tab1
     for (i = nbr - 1, j = 0; i >= 0; i--, j++)
         tab2[j] = tab1[i];
 
-    //Copie le tableau inversÈ dans l'original.
+    //Copie le tableau invers√© dans l'original.
     for (i = 0; i < nbr; i++)
         tab1[i] = tab2[i];
 
-    printf("Le tableau inversÈ est:\n ");
+    printf("Le tableau invers√© est:\n ");
 
     for (i = 0; i < nbr; i++)
         printf("%d\n", tab1[i]);
+   max=0;
+
+      for (int i=0 ; i< nbr ; i++)
+      {
+          if(tab1[i]>max){
+                         max=tab1[i];
+                         position=i;
+          }
+      }
+      printf("Maximum : %d Et sa Position : est %d\n",max,position);
+       min=tab1[0];
+      for (int i=0 ; i< nbr ; i++)
+      {
+          if(tab1[i]<min){
+                         min=tab1[i];
+                         position=i;
+          }
+      }
+      printf("Minimum : %d Et sa Position : est %d\n",min,position);
+      k=i;
+      for(i=1;i<=nbr-1;i++){
+        while(k>0&&tab1[k-1]>tab1[k]){
+            tmp=tab1[i];
+            tab1[k]=tab1[k-1];
+            tab1[k-1]=tmp;
+            k--;
+
+        }
+      }
+       printf("\n******** tableau tri√©e par ordre croissant ********\n");
+
+  for (i=0; i < nbr; i++)
+     printf("%d", tab1[i]);
 
     return 0;
+
 }
